a5241b7f3c4ee5cff79cf80c7015c99f2c0d360d
Mateo <mateolopez@unicesar.edu.co>
2026-08-21 23:28:35 -0500

    feat: add Programa entity

---
+++ AcademicTrack.Domain/Entities/Programa.cs
@@ -0,0 +1,10 @@
+﻿namespace AcademicTrack.Domain.Entities;
+
+public class Programa
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; }
+    public string? CodigoSnies { get; set; }
+    public string Facultad { get; set; }
+    public bool Activo { get; set; } = true;
+}
