21ae2e39a09392f510f6a8194d780d16b1975417
Mateo <mateolopez@unicesar.edu.co>
2026-08-22 00:38:13 -0500

    git commit -m "feat: add Publicacion, Autor and PublicacionAutor entities"

---
+++ AcademicTrack.Domain/Entities/Autor.cs
@@ -0,0 +1,7 @@
+﻿namespace AcademicTrack.Domain.Entities;
+
+public class Autor
+{
+    public int Id { get; set; }
+    public string NombreCompleto { get; set; } = string.Empty;
+}
---
+++ AcademicTrack.Domain/Entities/Publicacion.cs
@@ -0,0 +1,14 @@
+﻿namespace AcademicTrack.Domain.Entities;
+
+public class Publicacion
+{
+    public int Id { get; set; }
+    public int GrupoId { get; set; }
+    public string TipoPublicacion { get; set; } = string.Empty;
+    public short Anio { get; set; }
+    public string ReferenciaCompleta { get; set; } = string.Empty;
+    public string? Url { get; set; }
+    public bool? Indexada { get; set; }
+    public string? BaseIndexacion { get; set; }
+    public string? Cuartil { get; set; }
+}
