f5cdd313077d55a8c5ddcef26f2009184ffc61ea
Mateo <mateolopez@unicesar.edu.co>
2026-08-21 23:36:38 -0500

    feat: add ActividadExtension and ActividadExtensionVinculacion entities

---
+++ AcademicTrack.Domain/Entities/ActividadExtensionVinculacion.cs
@@ -0,0 +1,9 @@
+﻿namespace AcademicTrack.Domain.Entities;
+
+public class ActividadExtensionVinculacion
+{
+    public int Id { get; set; }
+    public int ActividadId { get; set; }
+    public string TipoActor { get; set; } = string.Empty; // "Estudiante", "Docente", "Egresado", "Administrativo"
+    public short Cantidad { get; set; } = 0;
+}
