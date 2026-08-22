c6231445c51cc30a2a2d3f423321c85cd7133228
Mateo <mateolopez@unicesar.edu.co>
2026-08-21 23:29:41 -0500

    feat: add PeriodoAcademico entity

---
+++ AcademicTrack.Domain/Entities/PeriodoAcademico.cs
@@ -0,0 +1,8 @@
+﻿namespace AcademicTrack.Domain.Entities;
+
+public class PeriodoAcademico
+{
+    public int Id { get; set; }
+    public short Anio { get; set; }
+    public string Semestre { get; set; } = string.Empty; // "I" o "II"
+}
