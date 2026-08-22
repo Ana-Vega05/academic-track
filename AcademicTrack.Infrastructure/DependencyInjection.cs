85327dfa52c2a61050199b376fc91e8fafef2134
Mateo <mateolopez@unicesar.edu.co>
2026-08-22 02:08:02 -0500

    feat: add DI registration for PostgreSQL

---
+++ AcademicTrack.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AcademicTrack.Infrastructure.Persistence;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("Default")
+                               ?? throw new InvalidOperationException("No se encontró la cadena de conexión 'Default'.");
+
+        services.AddDbContext<AcademicTrackDbContext>(options =>
+            options.UseNpgsql(connectionString));
+
+        return services;
+    }
+}
