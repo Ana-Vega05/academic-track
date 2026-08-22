fe0a612eeca73a13eefc376a6036da4c5f4f2e43
Mateo <mateolopez@unicesar.edu.co>
2026-08-21 23:34:48 -0500

    feat: add Innovacion and InnovacionVinculacion entities

---
+++ AcademicTrack.Domain/Entities/InnovacionVinculacion.cs
@@ -0,0 +1,10 @@
+﻿namespace AcademicTrack.Domain.Entities;
+
+public class InnovacionVinculacion
+{
+    public int Id { get; set; }
+    public int InnovacionId { get; set; }
+    public string TipoActor { get; set; } = string.Empty; // "Estudiante", "Docente", "Administrativo"
+    public string Condicion { get; set; } = "Interno"; // "Interno" o "Externo"
+    public short Cantidad { get; set; } = 0;
+}
