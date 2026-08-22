ed24334f282e20831e9212f8a31891892ef0f24e
Mateo <mateolopez@unicesar.edu.co>
2026-08-21 23:33:55 -0500

    feat: add ProyectoInvestigacion and ProyectoInvestigacionVinculacion entities

---
+++ AcademicTrack.Domain/Entities/ProyectoInvestigacion.cs
@@ -0,0 +1,15 @@
+﻿namespace AcademicTrack.Domain.Entities;
+
+public class ProyectoInvestigacion
+{
+    public int Id { get; set; }
+    public int GrupoId { get; set; }
+    public int? LineaId { get; set; }
+    public string Nombre { get; set; } = string.Empty;
+    public string InvestigadorPrincipal { get; set; } = string.Empty;
+    public string? ProductosGenerados { get; set; }
+    public string? UrlProducto { get; set; }
+    public string? ComunidadBeneficiada { get; set; }
+    public short? AnioInicio { get; set; }
+    public bool Vigente { get; set; } = true;
+}
---
+++ AcademicTrack.Domain/Entities/ProyectoInvestigacionVinculacion.cs
@@ -0,0 +1,9 @@
+﻿namespace AcademicTrack.Domain.Entities;
+
+public class ProyectoInvestigacionVinculacion
+{
+    public int Id { get; set; }
+    public int ProyectoId { get; set; }
+    public string TipoActor { get; set; } = string.Empty;
+    public short Cantidad { get; set; } = 0;
+}
