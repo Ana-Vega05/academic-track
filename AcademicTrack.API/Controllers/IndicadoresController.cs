6e1ab7265109df749f9895c5c213d2b65adafdc6
Mateo <mateolopez@unicesar.edu.co>
2026-08-22 21:32:53 -0500

    feat: add read-only indicadores catalog endpoint

---
+++ AcademicTrack.API/Controllers/IndicadoresController.cs
@@ -0,0 +1,27 @@
+﻿using AcademicTrack.Application.Metas.DTOs;
+using AcademicTrack.Application.Metas.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AcademicTrack.API.Controllers;
+
+[ApiController]
+[Route("api/indicadores")]
+public class IndicadoresController: ControllerBase
+{
+    private readonly IIndicadorRepository _repository;
+    public IndicadoresController(IIndicadorRepository repository) => _repository = repository;
+
+    [HttpGet]
+    public async Task<IActionResult> ObtenerTodos(CancellationToken cancellationToken)
+    {
+        var indicadores = await _repository.ObtenerTodosAsync(cancellationToken);
+        var dtos = indicadores.Select(i => new IndicadorDto
+        {
+            Id = i.Id,
+            Nombre = i.Nombre,
+            Unidad = i.Unidad,
+            Direccion = i.Direccion.ToString()
+        });
+        return Ok(dtos);
+    }
+}
