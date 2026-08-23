08f0ffdb5c6834b9c2cf43fa1773a74767ed3906
Mateo <mateolopez@unicesar.edu.co>
2026-08-22 21:03:54 -0500

    feat: add Meta, Indicador and MetaEvidencia entities

---
+++ AcademicTrack.Domain/Entities/MetaEvidencia.cs
@@ -0,0 +1,10 @@
+﻿namespace AcademicTrack.Domain.Entities;
+
+public class MetaEvidencia
+{
+    public int Id { get; set; }
+    public int MetaId { get; set; }
+    public string Descripcion { get; set; } = string.Empty;
+    public string? Url { get; set; }
+    public DateOnly FechaCarga { get; set; }
+}
